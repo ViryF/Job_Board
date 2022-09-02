0bee6011e0b0ea347896ca8e9d3f531a973fe460
Viry <diana13_2011@hotmail.com>
2022-09-02 13:50:19 -0500

    able to create employer

--- routes/employerRouter.js
+++ routes/employerRouter.js
@@ -2,7 +2,16 @@ const { Router } = require('express')
 const router = Router()
 const { employerController } = require('../controllers')
 
+// get routes
 router.get('/', employerController.getAllEmployers)
 router.get('/:id', employerController.getEmployerById)
+
+// post routes
+router.post('/new', employerController.createEmployer)
+
+// update routes
+
+// delete routes
+
 
 module.exports = router
